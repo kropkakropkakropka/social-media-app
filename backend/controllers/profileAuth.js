4e5b727efed163d036b63bd119fbe03d335ff66d
Kuba <on.abuk@protonmail.com>
2022-08-09 20:18:58 +0200

    registration, loging in and loging out

--- backend/controllers/profileAuth.js
+++ backend/controllers/profileAuth.js
@@ -8,18 +8,18 @@ const profileAuth = {
             const {firstname, lastname, username, password} = req.body;
             //const newUsername = username.toLowerCase().replace(/ /g, ''); //transfer the input to lowercase
             const checkUsername = await Profile.findOne({username: username}); //checks if username exists
-            const testhasla = "test12345";
+            //const testhasla = "test12345";
             //if it does throw an error
             if(checkUsername){
                 res.status(400).json({err: 'Username is alredy taken'})
             }
             //checks if password isnt too short
-            if(testhasla.length < 8){
+            if(password.length < 8){
                 res.status(400).json({err: 'Password must be at least 8 characters'})
             }
 
             //hash password
-            const hashPassword = bcrypt.hashSync(testhasla, 12);
+            const hashPassword = bcrypt.hashSync(password, 12);
 
             const newProfile = new Profile({
                 firstname, 
@@ -32,10 +32,20 @@ const profileAuth = {
             const refreshToken = createRefreshToken({id: newProfile._id});
 
             res.cookie('refreshtoken', refreshToken,{
-                httpOnly,
+                httpOnly: true,
                 path:"api/refresh_token",
                 maxAge: 24*30*60*60*1000
             });
+
+            await newProfile.save();
+
+            res.json({
+                message: "registered successfully",
+                accessToken,
+                profile:{
+                    ...newProfile._doc
+                }
+            })
 
         } catch (error) {
             res.status(500).json("error: " + error);
@@ -46,25 +56,51 @@ const profileAuth = {
             const {username, password} = req.body;
 
             const checkUsername = await Profile.findOne({username: username});
-            if(checkUsername){
-                if(checkUsername.password == password){
-                    res.json("User exists")
+
+            if(!checkUsername) return res.status(400).json({message: 'User doesnt exist'});
+
+            const comparePassword = await bcrypt.compare(password, checkUsername.password);
+
+            if(!comparePassword) return res.status(400).json({message: 'Wrong password'});
+
+            const accessToken = createAccessToken({id: checkUsername._id});
+            const refreshToken = createRefreshToken({id: checkUsername._id});
+
+            res.cookie('refreshtoken', refreshToken,{
+                httpOnly: true,
+                path: "/api/refresh_token",
+                maxAge: 24*30*60*60*1000
+            });
+
+            res.json({
+                message: "logined successfully",
+                accessToken,
+                profile:{
+                    ...checkUsername._doc
                 }
-            }
+            })
+
         } catch (error) {
             res.status(500).json("error: " + error);
         }
     },
     logout: async (req, res)=>{
         try {
-            
+            res.clearCookie('refreshtoken', {path: "/api/refresh_token"});
+            res.json({message: "Logged out"});
         } catch (error) {
             res.status(500).json("error: " + error);
         }
     },
     generateToken: async (req, res)=>{
         try {
-            
+            // const refreshToken = res.cookies.refreshtoken;
+            // if(!refreshToken) return res.status(400).json({message: "You Need to Login"});
+            // webtoken.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, async(error, res)=>{
+            //     if(error) return res.status(400).json({message: "aha"})
+
+            //     const profile = await Profile.findById(result.id);
+            // })
         } catch (error) {
             res.status(500).json("error: " + error);
         }
@@ -72,7 +108,7 @@ const profileAuth = {
 }
 
 const createAccessToken = (payload) =>{
-    return webtoken.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn:"1d"})
+    return webtoken.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn:"1d"}) //returns a web token as string
 }
 
 const createRefreshToken = (payload) =>{
--- backend/models/profile.model.js
+++ backend/models/profile.model.js
@@ -6,12 +6,12 @@ const profileSchema = new Schema({
     firstname: {type: String, required: true, minlength: 2, maxlength: 15},
     lastname: {type: String, required: true, minlength: 2, maxlength: 25},
     username: {type: String, required: true, unique: true, minlength: 3, maxlength: 15},
-    password: {type: String, required: true, minlength: 8, maxlength: 55},
+    password: {type: String, required: true, minlength: 8},
     desc: {type: String, maxlength: 155, default:''},
     profileImg: {type: String, default:''},
     xp: {type: Number, required: true, default: 0},
-    posts: {type: Object},
-    friends: {type: Object}
+    posts: [Object],
+    friends: [String]
 },{timestamps: true})
 
 const Profile = mongoose.model('Profile', profileSchema);

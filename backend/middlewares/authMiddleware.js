9c6f087167eea0477697968fab8f54b6e67f6ad5
Kuba <on.abuk@protonmail.com>
2022-08-30 18:07:12 +0200

    middleware that handles user authentication

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,27 @@
+import webtoken from 'jsonwebtoken';
+import Profile from '../models/profile.model';
+
+const protect = async (req, res, next) =>{
+    let token;
+    const authHeader = req.headers.authorization;
+
+    if(authHeader && authHeader.startsWith('Bearer')){
+        try {
+            token = authHeader.split(' ')[1]; //extract token string from authheader
+
+            const decoded = webtoken.verify(token, process.env.ACCESS_TOKEN_SECRET); //verified token returns profile id
+
+            req.profile = await Profile.findById(decoded.id).select('-password');
+
+            next() //hmm
+        } catch (error) {
+            res.status(401).json("error: Not authorized, invalid token");
+        }
+    }
+
+    if(!token){
+        res.status(401).json("error: Not authorized, no token found");
+    }
+}
+
+export {protect};
